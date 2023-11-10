3a69c1a814df9a8852fe454c624d48c7654e9d3b
EZECALVO91 <ezequielcalvo@live.com>
2023-11-09 23:38:15 -0300

    formulario armado con estilo

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const { log } = require("console");
+
 const express = require ("express");
 
 const path = require ("path")
@@ -6,10 +8,32 @@ const app = express ();
 
 app.use(express.static(path.join(__dirname, "public")));
 
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
 app.listen (3030, () => {
-    console.log("Servidor corriendo en el puerto 4200");
+    console.log("Servidor corriendo en el puerto 3030");
 });
 
 app.get ("/", function(req,res){
     res.sendFile(path.join (__dirname, "/views/home.html"))
 });
+
+app.get ("/login", function(req,res){
+    res.sendFile(path.join (__dirname, "/views/login.html"))
+});
+app.post("/login", (req,res) => {
+    console.log(req.body);
+res.redirect("/");
+})
+
+app.get ("/register", function(req,res){
+    res.sendFile(path.join (__dirname, "/views/register.html"))
+});
+app.post("/register", (req,res) => {
+    console.log(req.body);
+res.redirect("/");
+})
